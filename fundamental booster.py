dad07765ad92f77c086bc4eeeb65f483fc30496d
niharsheladiya <niharsheladiya1506@gmail.com>
2026-07-04 18:56:27 +0530

    Update fundamental booster.py

--- fundamental booster.py
+++ fundamental booster.py
@@ -7,7 +7,7 @@ favnum = int(input("Please enter your favourite number: "))
 
 print("\nThank you! Here is the information we collected: ")
 
-print(f"Name: {name} (Type: {type(name)}, Menory Address: {id(name)})")
+print(f"\nName: {name} (Type: {type(name)}, Menory Address: {id(name)})")
 print(f"Age: {age} (Type: {type(age)}, Menory Address: {id(age)})")
 print(f"Height: {height} (Type: {type(height)}, Menory Address: {id(height)})")
 print(f"Favourite Number: {favnum} (Type: {type(favnum)}, Menory Address: {id(favnum)})")
